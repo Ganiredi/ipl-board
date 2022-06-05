a49f3d5ba7f59ae2fe4a8a4afb78c6b5eea5d661
anvesh-lp <knanvesh.01@gmail.com>
2022-06-05 17:47:31 -0400

    Initial commit

---
+++ src/test/java/com/anvesh/iplboard/IplBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anvesh.iplboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
