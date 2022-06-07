ea11b086ccec0801038f6ca6f03f33e3e0767595
anvesh-lp <knanvesh.01@gmail.com>
2022-06-06 23:27:45 -0400

    Adding batch processor to load csv data into JPA

--- src/main/java/com/anvesh/iplboard/model/Match.java
+++ src/main/java/com/anvesh/iplboard/model/Match.java
@@ -3,17 +3,21 @@ package com.anvesh.iplboard.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
 public class Match {
+    @Id
     private long id;
     private String city;
     private LocalDate date;
     private String playerOfMatch;
     private String venue;
-    private String neutral_venue;
+    private String neutralVenue;
     private String team1;
     private String team2;
     private String tossWinner;
